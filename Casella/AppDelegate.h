f66a251c3ce563aee6feb3e98e1c28f193cc3753
Thecafremo <thecafremo@Nina.local>
2012-02-18 13:32:22 +0100

    Initial Commit

---
+++ Casella/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Casella
+//
+//  Created by Jorge Juan Pardo Peset on 18/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
